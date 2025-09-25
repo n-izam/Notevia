8290c8e223ffb62092c65b783efb318a4980ac50
nisam <abnizam4@gmail.com>
2025-09-25 21:48:07 +0530

    setup home page and template and handle static files

--- cores/views.py
+++ cores/views.py
@@ -9,4 +9,4 @@ class HouseView(View):
     def get(self, request, user_id):
         user = CustomUser.objects.get(id=user_id)
         
-        return render(request, 'cores/house.html', {"user_id": user_id, "user":  user})
+        return render(request, 'cores/home.html', {"user_id": user_id, "user":  user})
