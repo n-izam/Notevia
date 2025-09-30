0dfc914fe6c6990a08f723884e40e61feb8d7dad
nisam <abnizam4@gmail.com>
2025-09-30 17:37:35 +0530

    setup admin side dashboard and productlisting pages and create base html for extenting

--- adminpanel/urls.py
+++ adminpanel/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import AdminDashView
+from .views import AdminDashView, AdminProductView
 
 urlpatterns = [
     path('admindash/<int:user_id>/',AdminDashView.as_view() , name='admin-dash'),
+    path('adminproduct/<int:user_id>/', AdminProductView.as_view(), name='admin-product'),
 ]
