1d2704e4d9e66e51d3fba2f4b6cbc526feb71b3d
nisam <abnizam4@gmail.com>
2025-10-06 21:34:53 +0530

    category management and add offer to eaach category

--- adminpanel/urls.py
+++ adminpanel/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
-from .views import AdminDashView, AdminProductView, AdminCustomersView, AdminCategoryView, AddCategoryView, ViewVariantView, AddVariantView
+from .views import AdminDashView, AdminProductView, AdminCustomersView, AdminCategoryView, AddCategoryView, ViewVariantView, AddVariantView, TogglCategoryStatusView, AddCategoryOffer, EditCategoryOffer
 
 urlpatterns = [
     path('admindash/<int:user_id>/',AdminDashView.as_view() , name='admin-dash'),
-    path('adminproduct/<int:user_id>/', AdminProductView.as_view(), name='admin-product'),
-    path('admincustomers/<int:user_id>/', AdminCustomersView.as_view(), name='admin-customers'),
-    path('admincategory/<int:user_id>/', AdminCategoryView.as_view(), name='admin-category'),
+    path('adminproduct/', AdminProductView.as_view(), name='admin-product'),
+    path('admincustomers/', AdminCustomersView.as_view(), name='admin-customers'),
+    path('admincategory/', AdminCategoryView.as_view(), name='admin-category'),
     path('addcategory/',AddCategoryView.as_view(), name='add-category'),
     path('viewvariant/', ViewVariantView.as_view(), name='view-variant'),
     path('addvariant/', AddVariantView.as_view(), name='add-variant'),
+    path('toggle-status/<int:pk>/', TogglCategoryStatusView.as_view(), name='toggle_status'),
+    path('admincategory/<int:category_id>/addcategory-offer', AddCategoryOffer.as_view(), name='addcategory_offer'),
+    path('admincategory/<int:category_id>/editcategory-offer', EditCategoryOffer.as_view(), name='editcategory_offer'),
     
 ]
