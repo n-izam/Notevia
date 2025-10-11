0f294d38d6868ab9b1379b5f4d2c88dcd21a0b50
nisam <abnizam4@gmail.com>
2025-10-11 12:17:32 +0530

    nothing big create a view for user-side home page and product lidting page and also add templates also small change in customers.html the adminpanel app

--- cores/urls.py
+++ cores/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, ProductlistingView, ProductDetailsView
 
 
 urlpatterns = [
     path('home/<int:user_id>/', HomeView.as_view(), name='cores-home'),
+    path('shop_product_list/', ProductlistingView.as_view(), name='shop_products'),
+    path('shopproductdetails/', ProductDetailsView.as_view(), name='shop_productdetail')
+
 ]
--- cores/views.py
+++ cores/views.py
@@ -4,6 +4,7 @@ from accounts.models import CustomUser
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
+from adminpanel.models import Category, Offer, Product, ProductImage, Variant
 
 # Create your views here.
 
@@ -12,6 +13,47 @@ from django.views.decorators.cache import never_cache
 class HomeView(View):
 
     def get(self, request, user_id):
+        latest_products = Product.objects.filter(is_deleted=False, is_listed=True).order_by('-created_at')[:9]
+
+        product_with_image = []
+        for product in latest_products:
+            main_image = product.images.filter(is_main=True).first()
+            product_with_image.append({
+                "product": product,
+                "main_image": main_image 
+            })
         # user = CustomUser.objects.get(id=user_id)
+
+        context = {
+            "product_with_image":product_with_image,
+            "user_id": user_id,
+        }
         
-        return render(request, 'cores/home.html', {"user_id": user_id})
+        return render(request, 'cores/home.html', context)
+    
+class ProductlistingView(View):
+
+    def get(self, request):
+        
+        products = Product.objects.filter(is_deleted=False, is_listed=True).order_by('-created_at')
+
+        product_with_image = []
+        for product in products:
+            main_image = product.images.filter(is_main=True).first()
+            product_with_image.append({
+                "product": product,
+                "main_image": main_image
+            })
+
+            context = {
+                "product_with_image": product_with_image,
+                "user_id": request.user.id,
+            }
+
+        return render(request, 'cores/productlist.html', context)
+    
+class ProductDetailsView(View):
+
+    def get(self, request):
+
+        return render(request, 'cores/productdetail.html', {"user_id": request.user.id})
